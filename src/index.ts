7ce8c874a5d0d60aa4d6c7734171d841dd0dac75
luca <luca.marignati@edu.unito.it>
2022-11-26 10:50:36 +0100

    fix css

--- src/index.ts
+++ src/index.ts
@@ -10,42 +10,40 @@ const main = () => {
   const lamps: Array<LampType> = [];
 
   for (let i = 0; i < 20; i++){
-    lamps.push( {id:0, status: OFF, maxPower: Math.floor(Math.random() * 10) + 1});
+    lamps.push( {id:i, status: OFF, maxPower: Math.floor(Math.random() * 10) + 1});
   }
   
-  const iconAndInnerHtmlText = (lampButton: HTMLButtonElement, lampObj: Lamp, lampImg: any) => {
+  const updateIconAndInnerHtmlText = (lampHtmlObj: HTMLDivElement, lampObj: Lamp, lampImg: any) => {
     const lampImgage = document.createElement('img');
     lampImgage.src = lampImg;
-    lampButton.innerHTML = "";
-    lampButton.append(lampImgage);
-    lampButton.append(document.createElement('br'));
-    lampButton.append(lampObj.getValuePower().toString() + "/" + lampObj.getMaxPower().toString());
-    const rechargeButton = document.createElement('button');
-    lampButton.append(document.createElement('br'));
-    rechargeButton.innerHTML = 'Recharge';
+    lampHtmlObj.innerHTML = "";
+    lampHtmlObj.append(lampImgage);
+    lampHtmlObj.append(document.createElement('br'));
+    lampHtmlObj.append(lampObj.getValuePower().toString() + "/" + lampObj.getMaxPower().toString());
   }
 
   lamps.forEach( (lampData: LampType, index: number) => {
     const lamp = new Lamp(lampData.id, lampData.status, lampData.maxPower);
-    const lampButton: HTMLButtonElement = document.createElement('button');
-    lampButton.id = lampData.id.toString();
-    iconAndInnerHtmlText(lampButton, lamp, lampImg);
+    const lampHtmlObj: HTMLDivElement = document.createElement('div');
+    lampHtmlObj.id = lampData.id.toString();
+    lampHtmlObj.className = "bg-white float-left m-3";
+    updateIconAndInnerHtmlText(lampHtmlObj, lamp, lampImg);
    
     // listener
-    lampButton.addEventListener('click', function handleClick(event: MouseEvent) {
+    lampHtmlObj.addEventListener('click', function handleClick(event: MouseEvent) {
       event.preventDefault();
       if(lamp.getValuePower() >= lamp.getMaxPower()){
         lamp.setStatus(BROKEN);
-        iconAndInnerHtmlText(lampButton, lamp, lampBrokenImg);
+        updateIconAndInnerHtmlText(lampHtmlObj, lamp, lampBrokenImg);
       } else {
         lamp.incrementValuePower();
         if(lamp.getStatus() === ON){
           lamp.setStatus(OFF);
-          iconAndInnerHtmlText(lampButton, lamp, lampImg);
+          updateIconAndInnerHtmlText(lampHtmlObj, lamp, lampImg);
         }
         else {
           lamp.setStatus(ON);
-          iconAndInnerHtmlText(lampButton, lamp, lampYellowImg);
+          updateIconAndInnerHtmlText(lampHtmlObj, lamp, lampYellowImg);
         }
       }
     });
@@ -54,7 +52,7 @@ const main = () => {
     if((index > 3 && ((index % 4) == 0))) {
       lampContainer.appendChild(document.createElement("hr"));
     }
-    lampContainer.appendChild(lampButton);
+    lampContainer.appendChild(lampHtmlObj);
    
   });
 
